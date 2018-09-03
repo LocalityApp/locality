09f024611fc18fd98379abce76a1dbe3f03465dd
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-09-03 16:21:37 -0600

    Adding npm packages for Auth0 and express. Also add initial implementations for user login

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Parse application? Do we need this? 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars. 
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "layout"}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +50,31 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(PORT, function() {
+  console.log("App listening at localhost: " + PORT);
+});
+
+// Auth0 Authentication and Event Handler - How to set up with handlebars
+// $('document').ready(function() {
+
+//   let webAuth = new auth0.WebAuth({
+//     domain: process.env.AUTH0_DOMAIN,
+//     clientID: process.env.AUTH0_CLIENT_ID,
+//     // Will need to update this in process.env file when we have actual callback URL. Set to localhost for now.
+//     redirectUri: process.env.AUTH0_CALLBACK_URL,
+//     audience: 'https://' + process.env.AUTH0_DOMAIN + '/userinfo',
+//     responseType: 'token id_token',
+//     scope: 'openid'
+//   });
+
+//   let loginBtn = $('#btn-login');
+
+//   loginBtn.click(function(event) {
+//     event.preventDefault();
+//     webAuth.authorize();
+//   });
+
+// });
 
 module.exports = app;
