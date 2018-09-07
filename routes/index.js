a7ca443053e86460a4a02278161692511b47940e
Schuyler Ankele <schuyler.ankele@charter.com>
2018-09-07 15:08:44 -0600

    Eslint Add on

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 // Loading dotenv file. 
 require('dotenv').config();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { 
-    title: 'Express',
-    creds: process.env
-  });
+router.get('/login', function(req, res) {
+	res.render('login', { 
+		title: 'Express',
+		creds: process.env
+	});
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,57 +10,60 @@ const cors = require('cors');
 // Loading dotenv file. 
 require('dotenv').config();
 
+
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
+	throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
 }
 
 const corsOptions =  {
-  origin: 'http://localhost:3000'
+	origin: 'http://localhost:3000'
 };
 
 router.use(cors(corsOptions));
 
 const checkJwt = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
+	// Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+	}),
 
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
+	// Validate the audience and the issuer.
+	audience: process.env.AUTH0_AUDIENCE,
+	issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+	algorithms: ['RS256']
 });
 
 const checkScopes = jwtAuthz(['read:messages']);
 
 router.get('/public', function(req, res) {
-  res.json({
-    // Update this with public search page when functioning.
-    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
-  });
+	res.json({
+		// Update this with public search page when functioning.
+		message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+	});
 });
 
 router.get('/private', checkJwt, function(req, res) {
-  res.json({
-    // Update this with private serach page when functioning.
-    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-  });
+	res.json({
+		// Update this with private serach page when functioning.
+		message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+	});
 });
 
 // Determine if this is needed.
 router.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-  });
+	res.json({
+		message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+	});
 });
 
-router.use(function(err, req, res, next){
-  console.error(err.stack);
-  return res.status(err.status).json({ message: err.message });
+router.use(function(err, req, res){
+	if(err){
+		throw err.stack;
+	}
+	return res.status(err.status).json({ message: err.message });
 });
 
 module.exports = router;
