09aa9943628d9f6ec954dd17646d8688f85b2c2d
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-09-08 14:03:15 -0600

    Adding Auth0 functionalities.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,49 @@
 const express = require('express');
+const passport = require('passport');
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
 // Loading dotenv file. 
 require('dotenv').config();
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 	res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res) {
-	res.render('login', { 
-		title: 'Express',
-		creds: process.env
+router.get('/login', passport.authenticate('auth0', {
+	scope: 'openid email profile'
+}),
+	function (req, res) {
+		res.redirect("/");
 	});
+
+router.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+router.get('/callback',
+	passport.authenticate('auth0', {
+		failureRedirect: '/failure'
+	}),
+	function (req, res) {
+		res.redirect(req.session.returnTo || '/user');
+	}
+);
+
+router.get('/failure', function (req, res) {
+	var error = req.flash("error");
+	var error_description = req.flash("error_description");
+	req.logout();
+	res.render('failure', {
+		error: error[0],
+		error_description: error_description[0],
+	});
+});
+
+router.get('/user', ensureLoggedIn, function (req, res, next) {
+    res.render('user', {
+        user: req.user,
+        userProfile: JSON.stringify(req.user, null, '  ')
+    });
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,69 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
 
-// Auth0 
-const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwksRsa = require('jwks-rsa');
-const cors = require('cors');
-
-// Loading dotenv file. 
-require('dotenv').config();
-
-
-if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-	throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
-}
-
-const corsOptions =  {
-	origin: 'http://localhost:3000'
-};
-
-router.use(cors(corsOptions));
-
-const checkJwt = jwt({
-	// Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-	secret: jwksRsa.expressJwtSecret({
-		cache: true,
-		rateLimit: true,
-		jwksRequestsPerMinute: 5,
-		jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-	}),
-
-	// Validate the audience and the issuer.
-	audience: process.env.AUTH0_AUDIENCE,
-	issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-	algorithms: ['RS256']
-});
-
-const checkScopes = jwtAuthz(['read:messages']);
-
-router.get('/public', function(req, res) {
-	res.json({
-		// Update this with public search page when functioning.
-		message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
-	});
-});
-
-router.get('/private', checkJwt, function(req, res) {
-	res.json({
-		// Update this with private serach page when functioning.
-		message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-	});
-});
-
-// Determine if this is needed.
-router.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
-	res.json({
-		message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-	});
-});
-
-router.use(function(err, req, res){
-	if(err){
-		throw err.stack;
-	}
-	return res.status(err.status).json({ message: err.message });
+/* GET user profile. */
+router.get('/', ensureLoggedIn, function (req, res, next) {
+    res.render('user', {
+        user: req.user,
+        userProfile: JSON.stringify(req.user, null, '  ')
+    });
 });
 
 module.exports = router;
