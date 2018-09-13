e527a28291f88ef35011951f01ca731111c3e480
sarahamme <35412054+sarahamme@users.noreply.github.com>
2018-09-12 18:46:30 -0600

    added jquery ui  features

---
+++ public/public/style.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $("#sortable").sortable({
+      revert: true
+    }); 
+  
+  $('.ui-state-default').on('click', function(){
+    let list = $('.ui-state-default');
+      list.forEach(element =>{
+        console.log(element);
+    });       
+  });
+
+
+});
