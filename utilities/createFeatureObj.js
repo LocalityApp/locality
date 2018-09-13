f52b34fd4f5bd0a2783b8f81b04ec2e2ef50d729
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-09-13 17:51:51 -0600

    Drop it like it's hot

---
+++ utilities/createFeatureObj.js
@@ -0,0 +1,15 @@
+
+//
+module.exports = function createFeatureObj(busObj) {
+  var pt = {
+    "type": "Feature", 
+    "geometry": {
+      "type": "Point", 
+      "coordinates": [busObj.coordinates.longitude, busObj.coordinates.latitude]
+    },
+    'properties': {
+      'name': busObj.name
+    }
+  };
+  return pt;
+};
---
+++ utilities/kmeans.js
@@ -0,0 +1,23 @@
+
+//
+var turf = require('@turf/turf');
+
+//
+module.exports = function kMeansClustering(ptsJSON) {
+  var options = {numberOfClusters: 5};
+  var clustered = turf.clustersKmeans(ptsJSON, options);
+  getCentroids(clustered);
+};
+
+//
+function getCentroids(cluster) {
+    var centroids = {};
+    cluster.features.forEach(function(element) {
+        if (Object.keys(centroids).indexOf(element.properties.cluster) === -1) {
+            var centroidNumber = element.properties.cluster;
+            centroids[centroidNumber] = element.properties.centroid;
+        }
+    });
+    console.log(centroids);
+    // return centroids;
+}
