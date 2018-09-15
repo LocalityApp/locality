b4bbc728095dc113d5a7c7d7b0c164a4d30b21b0
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-09-15 09:17:14 -0600

    maybe undo?

---
+++ controllers/single_hood.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const content  = fs.readFileSync(__dirname + '/ZillowNeighborhoodsCO.json');
+const neighborhoods = JSON.parse(content);
+
+console.log(neighborhoods.features[300]);
+//nightlife
+// neighborhoods.features[251];
+// { type: 'Feature',
+//   geometry: { type: 'Polygon', coordinates: [ [Array] ] },
+//   properties: 
+//    { State: 'CO',
+//      County: 'Denver',
+//      City: 'Denver',
+//      Name: 'Central Business District',
+//      RegionID: '268632' } }
+
+// neighborhoods.features[252];
+// { type: 'Feature',
+//   geometry: { type: 'Polygon', coordinates: [ [Array] ] },
+//   properties: 
+//    { State: 'CO',
+//      County: 'Denver',
+//      City: 'Denver',
+//      Name: 'City Park West',
+//      RegionID: '268637' } }
+
+// 260
+// { type: 'Feature',
+//   geometry: { type: 'Polygon', coordinates: [ [Array] ] },
+//   properties: 
+//    { State: 'CO',
+//      County: 'Denver',
+//      City: 'Denver',
+//      Name: 'Globeville',
+//      RegionID: '268664' } }
+
+// 271
+// { type: 'Feature',
+//   geometry: { type: 'Polygon', coordinates: [ [Array] ] },
+//   properties: 
+//    { State: 'CO',
+//      County: 'Boulder',
+//      City: 'Boulder',
+//      Name: 'Sale Lake',
+//      RegionID: '416123' } }
+
+// 202 - parks
+// { type: 'Feature',
+//   geometry: { type: 'Polygon', coordinates: [ [Array] ] },
+//   properties: 
+//    { State: 'CO',
+//      County: 'Denver',
+//      City: 'Denver',
+//      Name: 'Cherry Creek',
+//      RegionID: '268635' } }
